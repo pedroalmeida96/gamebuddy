0866fae2560317b0143bc549608d5974e9e9c2c6
Pedro Almeida <pedro.gregorio96@gmail.com>
2023-06-17 11:16:42 +0100

    add user

--- src/main/java/com/pedroalmeida/gamebuddy/controller/UserController.java
+++ src/main/java/com/pedroalmeida/gamebuddy/controller/UserController.java
@@ -4,6 +4,7 @@ import com.pedroalmeida.gamebuddy.model.AppUser;
 import com.pedroalmeida.gamebuddy.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,12 +17,13 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<AppUser> getAllStudents() {
+    public List<AppUser> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @PostMapping("/create")
-    public void addStudent(@Valid @RequestBody AppUser appUser) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addUser(@Valid @RequestBody AppUser appUser) {
         userService.addUser(appUser);
     }
 }
