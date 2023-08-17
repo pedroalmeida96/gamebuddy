33ee1cb902a000f951db5e2d9fe027e4ea4a0f59
Pedro Almeida <pedro.gregorio96@gmail.com>
2023-08-17 08:47:49 +0100

    AppUser takes care of login and registration

--- src/main/java/com/pedroalmeida/gamebuddy/security/AuthenticationController.java
+++ src/main/java/com/pedroalmeida/gamebuddy/security/AuthenticationController.java
@@ -1,51 +1,24 @@
 package com.pedroalmeida.gamebuddy.security;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
 import com.pedroalmeida.gamebuddy.appuser.AppUser;
 import lombok.AllArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-
-import static com.pedroalmeida.gamebuddy.security.FilterToken.SECRET;
-
 @RestController
 @AllArgsConstructor
 public class AuthenticationController {
-    private final AuthenticationManager authenticationManager;
     private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
     public String login(@RequestBody Login login) {
-        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                new UsernamePasswordAuthenticationToken(login.getUsername(), login.getPassword());
-
-        Authentication authenticate = this.authenticationManager.authenticate(usernamePasswordAuthenticationToken);
-
-        var usuario = (AppUser) authenticate.getPrincipal();
-
-        return JWT.create()
-                .withSubject(usuario.getUsername())
-                .withClaim("id", usuario.getUserId())
-                .withExpiresAt(LocalDateTime.now()
-                        .plusMinutes(60)
-                        .toInstant(ZoneOffset.of("+01:00"))
-                )
-                .sign(Algorithm.HMAC256(SECRET));
-
+        return authenticationService.loginUser(login);
     }
 
     @PostMapping("/registration")
     public AppUser registration(@RequestBody Login login) {
-        AppUser appUser = AppUser.builder().username(login.getUsername()).name(login.getName()).password(login.getPassword()).build();
-        return authenticationService.registerUser(appUser);
+        return authenticationService.registerUser(login);
     }
 }
 
--- src/main/java/com/pedroalmeida/gamebuddy/security/AuthenticationService.java
+++ src/main/java/com/pedroalmeida/gamebuddy/security/AuthenticationService.java
@@ -1,17 +1,28 @@
 package com.pedroalmeida.gamebuddy.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.pedroalmeida.gamebuddy.appuser.AppUser;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import static com.pedroalmeida.gamebuddy.security.FilterToken.SECRET;
+
 @Service
 @AllArgsConstructor
 public class AuthenticationService implements UserDetailsService {
 
+    private final AuthenticationManager authenticationManager;
     private final AuthenticationRepository authenticationRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -25,11 +36,26 @@ public class AuthenticationService implements UserDetailsService {
         return appUser;
     }
 
-    public AppUser registerUser(AppUser appUser) {
+    public AppUser registerUser(Login login) {
+        AppUser appUser = AppUser.builder().username(login.getUsername()).name(login.getName()).password(login.getPassword()).build();
         if (authenticationRepository.findByUsername(appUser.getUsername()) != null) {
             throw new UserAlreadyExistsException("Username already exists in db.");
         }
         appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
         return authenticationRepository.save(appUser);
+    }
+
+    public String loginUser(Login login) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(login.getUsername(), login.getPassword());
+        Authentication authenticate = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        var usuario = (AppUser) authenticate.getPrincipal();
+        return JWT.create()
+                .withSubject(usuario.getUsername())
+                .withClaim("id", usuario.getUserId())
+                .withExpiresAt(LocalDateTime.now()
+                        .plusMinutes(60)
+                        .toInstant(ZoneOffset.of("+01:00"))
+                )
+                .sign(Algorithm.HMAC256(SECRET));
     }
 }
