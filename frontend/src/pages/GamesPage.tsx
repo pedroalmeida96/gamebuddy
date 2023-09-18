0a37e37782c1a3faa718b3be25cd3ff85c3caafa
Pedro Almeida <pedro.gregorio96@gmail.com>
2023-09-18 12:55:08 +0100

    POST working

--- frontend/src/pages/GamesPage.tsx
+++ frontend/src/pages/GamesPage.tsx
@@ -1,5 +1,20 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/games";
+const INITIAL_FORM_DATA = {
+  gameType: "FOOTBALL",
+  location: "CONA",
+  gameDateTime: "2024-08-25T15:00:00",
+  participants: [
+    {
+      userId: "1",
+      name: "Alice",
+    },
+  ],
+};
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsImlkIjoxLCJleHAiOjE2OTUwNDA2ODR9.WPmdv4GrUadSf-GfUhWijHLdLZ_IO3yy91uAY4KXmuc";
 
 interface Game {
   gameId: number;
@@ -8,25 +23,66 @@ interface Game {
 }
 
 function MyGamesPage() {
-  const [games, setGames] = useState([]);
-  const token = "TOKEN_HERE";
+  const [games, setGames] = useState<Game[]>([]);
+  const [formData, setFormData] = useState(INITIAL_FORM_DATA);
 
-  useEffect(() => {
+  const fetchGames = () => {
     const headers = {
-      Authorization: token,
+      Authorization: TOKEN,
     };
-    const url = "http://localhost:8080/api/games";
+
     axios
-      .get(url, { headers })
+      .get(API_URL, { headers })
       .then((response) => {
-        // Update the state with the retrieved data
-        console.log(response.data);
         setGames(response.data);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, [token]);
+  };
+
+  useEffect(() => {
+    fetchGames();
+  }, []);
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(API_URL + "/create", formData, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: TOKEN,
+        },
+      });
+
+      if (response.status === 200) {
+        // After successfully creating the game, fetch the updated list of games
+        const gamesResponse = await axios.get(API_URL, {
+          headers: {
+            Authorization: TOKEN,
+          },
+        });
+
+        if (gamesResponse.status === 200) {
+          fetchGames();
+          setFormData(INITIAL_FORM_DATA); // Reset the form after successful submission
+        }
+      }
+    } catch (error) {
+      console.error("Error creating the game:", error);
+    }
+  };
 
   return (
     <div>
@@ -34,11 +90,39 @@ function MyGamesPage() {
       <ul>
         {games.map((game: Game) => (
           <li key={game.gameId}>
-            <strong>Location:</strong> {game.location}, <strong>Genre:</strong>{" "}
-            {game.gameDateTime}
+            <strong>Location:</strong> {game.location},{" "}
+            <strong>Game Date and Time:</strong> {game.gameDateTime}
           </li>
         ))}
       </ul>
+      <div>
+        <h1>Insert new game</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="location">Location:</label>
+            <input
+              type="text"
+              id="location"
+              name="location"
+              value={formData.location}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="gameDateTime">Game Date and Time:</label>
+            <input
+              type="datetime-local"
+              id="gameDateTime"
+              name="gameDateTime"
+              value={formData.gameDateTime}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <button type="submit">Create Game</button>
+        </form>
+      </div>
     </div>
   );
 }
