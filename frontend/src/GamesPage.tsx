83e85d1c744864169a92a0a44c073ec679444c73
Pedro Almeida <pedro.gregorio96@gmail.com>
2023-09-21 11:57:26 +0100

    update working with cleaner code

--- frontend/src/GamesPage.tsx
+++ frontend/src/GamesPage.tsx
@@ -1,127 +1,86 @@
-import { Component, ChangeEvent } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
+import axios from "axios";
 import Game from "./types/game";
 import GameType from "./types/gameType";
-import GameTypes from "./gamestypelist.component copy";
-import axios from "axios";
 import GamesList from "./gameslist.component";
+import GameTypes from "./gamestypelist.component";
 
-type Props = {
-};
+type GamesPageProps = {};
 
-type State = {
-  games: Array<Game>;
-  gameTypes: Array<GameType>;
-  currentGame: Game | null;
-  currentIndex: number;
-  gameId: any;
-  location: string;
-  gameDateTime: string;
-  selectedGameType: string;
-  isCreatingNewGame: boolean;
-  isEditingGame: boolean;
-};
+function GamesPage(props: GamesPageProps) {
+  const [games, setGames] = useState<Array<Game>>([]);
+  const [gameTypes, setGameTypes] = useState<Array<GameType>>([]);
+  const [currentGame, setCurrentGame] = useState<Game | null>(null);
+  const [gameId, setGameId] = useState<any>("");
+  const [location, setLocation] = useState<string>("");
+  const [gameDateTime, setGameDateTime] = useState<string>("");
+  const [selectedGameType, setSelectedGameType] = useState<string>("");
+  const [isCreatingNewGame, setIsCreatingNewGame] = useState<boolean>(false);
+  const [isEditingGame, setIsEditingGame] = useState<boolean>(false);
 
-export default class GamesPage extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.onChangeLocation = this.onChangeLocation.bind(this);
-    this.onChangeDate = this.onChangeDate.bind(this);
-    this.saveGame = this.saveGame.bind(this);
-    this.retrieveGames = this.retrieveGames.bind(this);
-    this.retrieveGameTypes = this.retrieveGameTypes.bind(this);
-    this.selectGame = this.selectGame.bind(this);
+  useEffect(() => {
+    retrieveGames();
+    retrieveGameTypes();
+  }, []);
 
-    this.state = {
-      gameId: "",
-      games: [],
-      gameTypes: [],
-      currentGame: null,
-      currentIndex: -1,
-      location: "",
-      gameDateTime: "",
-      selectedGameType: "",
-      isCreatingNewGame: false,
-      isEditingGame: false,
-    };
-  }
+  const toggleCreateFields = () => {
+    setIsCreatingNewGame(!isCreatingNewGame);
+    setIsEditingGame(false);
+    setCurrentGame(null);
+  };
 
-  componentDidMount() {
-    this.retrieveGames();
-    this.retrieveGameTypes();
-  }
 
-  toggleCreateFields() {
-    this.setState({
-      isCreatingNewGame: !this.state.isCreatingNewGame,
-      isEditingGame: false,
-      currentGame: null,
-    });
-  }
-
-  toggleEditFields() {
-    this.setState({
-      isEditingGame: !this.state.isEditingGame,
-      isCreatingNewGame: false,
-    });
-  }
-
-  retrieveGames() {
-    axios.get("http://localhost:8080/api/games", {
-      headers: {
-        Authorization: localStorage.getItem("token"),
-        "Content-type": "application/json"
-      }
-    })
+  const retrieveGames = () => {
+    axios
+      .get("http://localhost:8080/api/games", {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+          "Content-type": "application/json",
+        },
+      })
       .then((response: any) => {
-        this.setState({
-          games: response.data,
-        });
+        setGames(response.data);
         console.log(response.data);
       })
       .catch((e: Error) => {
         console.log(e);
       });
-  }
+  };
 
-  retrieveGameTypes() {
-    axios.get("http://localhost:8080/api/gameTypes", {
-      headers: {
-        Authorization: localStorage.getItem("token"),
-        "Content-type": "application/json"
-      }
-    })
+  const retrieveGameTypes = () => {
+    axios
+      .get("http://localhost:8080/api/gameTypes", {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+          "Content-type": "application/json",
+        },
+      })
       .then((response: any) => {
         const gameTypes = response.data.map((sportsName: string) => ({
           sportsName,
         }));
-
-        this.setState({
-          gameTypes,
-        });
+        setGameTypes(gameTypes);
         console.log("gametype", gameTypes);
       })
       .catch((e: Error) => {
         console.log("Error:", e);
       });
-  }
+  };
 
-  onChangeLocation(e: ChangeEvent<HTMLInputElement>) {
-    this.setState({
-      location: e.target.value,
-    });
-  }
+  const onChangeLocation = (e: ChangeEvent<HTMLInputElement>) => {
+    setLocation(e.target.value);
+  };
 
-  onChangeDate(e: ChangeEvent<HTMLInputElement>) {
-    this.setState({
-      gameDateTime: e.target.value,
-    });
-  }
+  const onChangeDate = (e: ChangeEvent<HTMLInputElement>) => {
+    setGameDateTime(e.target.value);
+  };
 
-  saveGame() {
+
+  const saveGame = () => {
     const data: Game = {
-      location: this.state.location,
-      gameDateTime: this.state.gameDateTime,
-      gameType: this.state.selectedGameType,
+      location: location,
+      gameDateTime: gameDateTime,
+      gameType: selectedGameType,
       participants: [
         {
           userId: "1",
@@ -130,91 +89,48 @@ export default class GamesPage extends Component<Props, State> {
       ],
     };
 
-    axios.post("http://localhost:8080/api/games/create", data, {
-      headers: {
-        Authorization: localStorage.getItem("token"),
-        "Content-type": "application/json"
-      }
-    })
+    axios
+      .post("http://localhost:8080/api/games/create", data, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+          "Content-type": "application/json",
+        },
+      })
       .then((response: any) => {
-        this.setState({
-          gameId: response.data.gameId,
-          location: "",
-          gameDateTime: "",
-          selectedGameType: ""
-        });
+        setGameId(response.data.gameId);
+        setLocation("");
+        setGameDateTime("");
+        setSelectedGameType("");
         console.log(response.data);
-        this.retrieveGames();
+        retrieveGames();
       })
       .catch((e: Error) => {
         console.log(e);
       });
-  }
+  };
 
-  selectGame(game: Game) {
-    this.setState({
-      currentGame: game,
-      location: game.location,
-      gameDateTime: game.gameDateTime,
-      selectedGameType: game.gameType,
-    });
-  }
 
-  render() {
-    const { gameTypes, games, location, gameDateTime, currentGame, isCreatingNewGame, isEditingGame, } = this.state;
-
-    return (
-      <div>
-        <h2>GAMES</h2>
-        <button onClick={() => this.toggleCreateFields()}>Create New Game</button>
-        <GamesList gamesList={games} retrieveGames={this.retrieveGames} />
-        {isCreatingNewGame && ( // Render create fields if isCreatingNewGame is true
-          <div>
-            <h3>Create New Game</h3>
-            <div>
-              <div>
-                <div>
-                  <label htmlFor="location">Location</label>
-                  <input type="text" id="location" required value={location} onChange={this.onChangeLocation} name="location" />
-                </div>
-                <div>
-                  <label htmlFor="gameDateTime">Game DateTime</label>
-                  <input type="datetime-local" id="gameDateTime" required value={gameDateTime} onChange={this.onChangeDate} name="gameDateTime" />
-                </div>
-                <div>
-                  <label htmlFor="gameType">Game Type</label>
-                  <select id="gameType" value={this.state.selectedGameType} onChange={(e) => this.setState({ selectedGameType: e.target.value })}>
-                    <option value="">Select a game type</option>
-                    {gameTypes &&
-                      gameTypes.map((gameType, index) => (
-                        <option key={index} value={gameType.sportsName}>
-                          {gameType.sportsName}
-                        </option>
-                      ))}
-                  </select>
-                </div>
-                <button onClick={() => this.saveGame()}>Create</button>
-              </div>
-            </div>
-          </div>
-        )}
-
+  return (
+    <div>
+      <h2>GAMES</h2>
+      <button onClick={toggleCreateFields}>Create New Game</button>
+      <GamesList gamesList={games} retrieveGames={retrieveGames} gameTypes={gameTypes} />
+      {isCreatingNewGame && (
         <div>
-          {currentGame && isEditingGame && (
+          <h3>Create New Game</h3>
+          <div>
             <div>
-              <h3>Edit Game</h3>
               <div>
                 <label htmlFor="location">Location</label>
-                <input
-                  type="text" id="location" required value={location} onChange={this.onChangeLocation} name="location" />
+                <input type="text" id="location" required value={location} onChange={onChangeLocation} name="location" />
               </div>
               <div>
                 <label htmlFor="gameDateTime">Game DateTime</label>
-                <input type="datetime-local" id="gameDateTime" required value={gameDateTime} onChange={this.onChangeDate} name="gameDateTime" />
+                <input type="datetime-local" id="gameDateTime" required value={gameDateTime} onChange={onChangeDate} name="gameDateTime" />
               </div>
               <div>
                 <label htmlFor="gameType">Game Type</label>
-                <select id="gameType" value={this.state.selectedGameType} onChange={(e) => this.setState({ selectedGameType: e.target.value })} >
+                <select id="gameType" value={selectedGameType} onChange={(e) => setSelectedGameType(e.target.value)}>
                   <option value="">Select a game type</option>
                   {gameTypes &&
                     gameTypes.map((gameType, index) => (
@@ -224,14 +140,14 @@ export default class GamesPage extends Component<Props, State> {
                     ))}
                 </select>
               </div>
-              <button onClick={this.saveGame}>Update</button>
+              <button onClick={saveGame}>Create</button>
             </div>
-          )}
+          </div>
         </div>
-
-        <GameTypes gameTypeList={this.state.gameTypes} />
-      </div>
-    );
-  }
+      )}
+      <GameTypes gameTypeList={gameTypes} />
+    </div>
+  );
 }
 
+export default GamesPage;
--- frontend/src/gameslist.component.tsx
+++ frontend/src/gameslist.component.tsx
@@ -1,24 +1,30 @@
+import React, { useState } from "react";
 import axios from "axios";
 import Game from "./types/game";
+import GameType from "./types/gameType";
 
 type GamesListProps = {
     gamesList: Array<Game>;
+    gameTypes: Array<GameType>;
     retrieveGames: () => void;
 };
 
-function GamesList({ gamesList, retrieveGames }: GamesListProps) {
+function GamesList({ gamesList, gameTypes, retrieveGames }: GamesListProps) {
+    const [editingGame, setEditingGame] = useState<Game | null>(null);
 
     const editGame = (game: Game) => {
-        console.log(game.gameId);
+        console.log(game);
+        setEditingGame(game);
     };
 
     const deleteGame = (gameId: any) => {
-        axios.delete("http://localhost:8080/api/games/delete/" + gameId, {
-            headers: {
-                Authorization: localStorage.getItem("token"),
-                "Content-type": "application/json"
-            }
-        })
+        axios
+            .delete("http://localhost:8080/api/games/delete/" + gameId, {
+                headers: {
+                    Authorization: localStorage.getItem("token"),
+                    "Content-type": "application/json",
+                },
+            })
             .then(() => {
                 console.log("Game deleted successfully");
                 retrieveGames();
@@ -26,6 +32,59 @@ function GamesList({ gamesList, retrieveGames }: GamesListProps) {
             .catch((e: Error) => {
                 console.log(e);
             });
+    };
+
+    const cancelEdit = () => {
+        setEditingGame(null);
+    };
+
+    const updateGame = () => {
+        if (editingGame) {
+
+            const data: Game = {
+                gameId: editingGame.gameId,
+                location: editingGame.location,
+                gameDateTime: editingGame.gameDateTime,
+                gameType: editingGame.gameType,
+                participants: [
+                    {
+                        userId: "1",
+                        name: "Alice",
+                    },
+                ],
+            };
+
+            axios
+                .put(
+                    "http://localhost:8080/api/games/update", data,
+                    {
+                        headers: {
+                            Authorization: localStorage.getItem("token"),
+                            "Content-type": "application/json",
+                        },
+                    }
+                )
+                .then(() => {
+                    console.log("Game updated successfully");
+                    retrieveGames();
+                    setEditingGame(null); // Reset the editing state
+                })
+                .catch((e: Error) => {
+                    console.log(e);
+                });
+        }
+    };
+
+    const handleInputChange = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+    ) => {
+        if (editingGame) {
+            const { name, value } = event.target;
+            setEditingGame({
+                ...editingGame,
+                [name]: value,
+            });
+        }
     };
 
     return (
@@ -36,14 +95,64 @@ function GamesList({ gamesList, retrieveGames }: GamesListProps) {
                     gamesList.map((game, index) => (
                         <li key={index}>
                             <span>
-                                {game.gameId}, {game.location}, {game.gameDateTime}
+                                {game.gameId}, {game.location}, {game.gameDateTime}, {game.gameType}
                             </span>
                             <button onClick={() => deleteGame(game.gameId)}>Delete</button>
-                            <button onClick={() => editGame(game)}>Edit</button> {/* Add Edit button */}
+                            <button onClick={() => editGame(game)}>Edit</button>
                         </li>
                     ))}
             </ul>
+            {editingGame && (
+                <div>
+                    <h3>Edit Game</h3>
+                    <div>
+                        <label htmlFor="gameId">Game Id</label>
+                        <input
+                            type="text"
+                            id="gameId"
+                            required
+                            value={editingGame.gameId}
+                            name="gameId"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="location">Location</label>
+                        <input
+                            type="text"
+                            id="location"
+                            name="location"
+                            value={editingGame.location}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="gameDateTime">Game DateTime</label>
+                        <input
+                            type="datetime-local"
+                            id="gameDateTime"
+                            name="gameDateTime"
+                            value={editingGame.gameDateTime}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="gameType">Game Type</label>
+                        <select id="gameType" value={editingGame.gameType} onChange={handleInputChange} name="gameType">
+                            <option value="">Select a game type</option>
+                            {gameTypes &&
+                                gameTypes.map((gameType, index) => (
+                                    <option key={index} value={gameType.sportsName}>
+                                        {gameType.sportsName}
+                                    </option>
+                                ))}
+                        </select>
+                    </div>
+                    <button onClick={updateGame}>Update</button>
+                    <button onClick={cancelEdit}>Cancel</button>
+                </div>
+            )}
         </div>
     );
 }
+
 export default GamesList;
