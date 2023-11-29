553b000cbff700d4d34906553c4467a0bad05db1
Pedro Almeida <pedro.gregorio96@gmail.com>
2023-11-29 19:19:38 +0000

    React bootstrap added

---
+++ frontend/src/editgame.modal.tsx
@@ -0,0 +1,136 @@
+import { useState, ChangeEvent } from "react";
+import axios from "axios";
+import { Modal, Button } from "react-bootstrap";
+import Game from "./types/game";
+import GameType from "./types/gameType";
+import AppUser from "./types/appuser";
+
+interface EditGameModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    retrieveGames: () => void;
+    gameTypes: Array<GameType>;
+    users: Array<AppUser>;
+}
+
+function EditGameModal(props: EditGameModalProps) {
+    const [location, setLocation] = useState<string>("");
+    const [gameDateTime, setGameDateTime] = useState<string>("");
+    const [selectedGameType, setSelectedGameType] = useState<string>("");
+    const [selectedUsers, setSelectedUsers] = useState<Array<AppUser>>([]);
+
+    const onChangeLocation = (e: ChangeEvent<HTMLInputElement>) => {
+        setLocation(e.target.value);
+    };
+
+    const onChangeDate = (e: ChangeEvent<HTMLInputElement>) => {
+        setGameDateTime(e.target.value);
+    };
+
+    const saveGame = (game: Game) => {
+        axios
+            .post("http://localhost:8080/api/games/create", game, {
+                headers: {
+                    Authorization: localStorage.getItem("token"),
+                    "Content-type": "application/json",
+                },
+            })
+            .then((response: any) => {
+                setLocation("");
+                setGameDateTime("");
+                setSelectedGameType("");
+                setSelectedUsers([]);
+                console.log(response.data);
+                props.retrieveGames();
+                props.onClose(); // Close the modal after creating the game
+            })
+            .catch((e: Error) => {
+                console.log(e);
+            });
+    };
+
+    function handleChange(event: ChangeEvent<HTMLSelectElement>): void {
+        const selectedIds = Array.from(
+            event.target.selectedOptions,
+            (option) => option.value
+        );
+        const selectedUserObjects = props.users.filter((user) =>
+            selectedIds.includes(user.userId.toString())
+        );
+        setSelectedUsers(selectedUserObjects);
+    }
+
+    return (
+        <Modal show={props.isOpen} onHide={props.onClose} centered>
+            <Modal.Header closeButton>
+                <Modal.Title>Create New Game</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div>
+                    <label htmlFor="location">Location</label>
+                    <input
+                        type="text"
+                        id="location"
+                        required
+                        value={location}
+                        onChange={onChangeLocation}
+                        name="location"
+                        className="form-control"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="gameDateTime">Game DateTime</label>
+                    <input
+                        type="datetime-local"
+                        id="gameDateTime"
+                        required
+                        value={gameDateTime}
+                        onChange={onChangeDate}
+                        name="gameDateTime"
+                        className="form-control"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="gameType">Game Type</label>
+                    <select
+                        id="gameType"
+                        value={selectedGameType}
+                        onChange={(e) => setSelectedGameType(e.target.value)}
+                        className="form-control"
+                    >
+                        <option value="">Select a game type</option>
+                        {props.gameTypes &&
+                            props.gameTypes.map((gameType, index) => (
+                                <option key={index} value={gameType.sportsName}>
+                                    {gameType.sportsName}
+                                </option>
+                            ))}
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="users">Participants</label>
+                    <select
+                        id="users"
+                        multiple
+                        value={selectedUsers.map((user) => user.userId)}
+                        onChange={handleChange}
+                        className="form-control"
+                    >
+                        {props.users &&
+                            props.users.map((user, index) => (
+                                <option key={index} value={user.userId}>
+                                    {user.userId} {user.name}
+                                </option>
+                            ))}
+                    </select>
+                </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={props.onClose}>Cancel</Button>
+                <Button variant="primary" onClick={() => saveGame({ location, gameDateTime, gameType: selectedGameType, participants: selectedUsers })}>Create</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default EditGameModal;
