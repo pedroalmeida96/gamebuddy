901d2d13b188d1dfd9564223b90e057e8be9379d
Pedro Almeida <pedro.gregorio96@gmail.com>
2023-12-17 10:24:32 +0000

    Favorites

--- src/main/java/com/pedroalmeida/gamebuddy/game/FavoritesController.java
+++ src/main/java/com/pedroalmeida/gamebuddy/game/FavoritesController.java
@@ -1,13 +1,12 @@
 package com.pedroalmeida.gamebuddy.game;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
