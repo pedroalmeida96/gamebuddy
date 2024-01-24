3a951ad619f0f19dff31eb431d31bf59337006b4
pedroalmeida96 <59938109+pedroalmeida96@users.noreply.github.com>
2024-01-24 07:25:08 +0000

    Other improvement (#35)
    * other-improvement
    
    * other-improvement
    
    * other-improvement
    
    * improvements and ExceptionHandler

--- frontend/src/registration.component.tsx
+++ frontend/src/registration.component.tsx
@@ -3,24 +3,13 @@ import { Form, Button, Container, Row, Col, Alert } from "react-bootstrap";
 import axios from "axios";
 import "./styles.css";
 
-type SetIsLoggedInType = (value: boolean) => void;
-
-interface RegistrationProps {
-  setIsLoggedIn: SetIsLoggedInType;
-}
-
-function Registration({ setIsLoggedIn }: RegistrationProps) {
+function Registration() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
   const handleRegistration = () => {
-    if (password !== confirmPassword) {
-      setErrorMessage("Passwords do not match.");
-      return;
-    }
-
     const requestData = {
       username: username,
       name: username,
@@ -30,12 +19,14 @@ function Registration({ setIsLoggedIn }: RegistrationProps) {
     axios
       .post("http://localhost:8080/registration", requestData)
       .then((response) => {
-        if (response.status === 201) {
+        if (response.status == 200) {
           localStorage.setItem("username", requestData.username);
           localStorage.setItem("token", response.data);
-          setIsLoggedIn(true);
+          setSuccessMessage("Registration succeeded.");
+          setErrorMessage(null);
         } else {
           setErrorMessage("Registration failed.");
+          setSuccessMessage(null);
         }
       })
       .catch((error) => {
@@ -50,6 +41,7 @@ function Registration({ setIsLoggedIn }: RegistrationProps) {
         <Col xs={12} md={6}>
           <h2>Registration</h2>
           {errorMessage && <Alert variant="danger" className="error-message">{errorMessage}</Alert>}
+          {successMessage && <Alert variant="success" className="success-message">{successMessage}</Alert>}
           <Form className="registration-form">
             <Form.Group controlId="formUsername">
               <Form.Label>Username</Form.Label>
@@ -70,20 +62,7 @@ function Registration({ setIsLoggedIn }: RegistrationProps) {
                 onChange={(e) => setPassword(e.target.value)}
               />
             </Form.Group>
-
-            <Form.Group controlId="formConfirmPassword">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Confirm your password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-            </Form.Group>
-
-            <Button variant="primary" onClick={handleRegistration}>
-              Register
-            </Button>
+            <Button variant="primary" onClick={handleRegistration}>Register</Button>
           </Form>
         </Col>
       </Row>
