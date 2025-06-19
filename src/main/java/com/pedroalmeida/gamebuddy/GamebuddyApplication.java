dc19b736ca9443c91b35f14e9d3cc667da9a5668
pedro.1.almeida <pedro.1.almeida@nokia.com>
2025-06-19 15:39:39 +0100

    frontend packaging

--- src/main/java/com/pedroalmeida/gamebuddy/GamebuddyApplication.java
+++ src/main/java/com/pedroalmeida/gamebuddy/GamebuddyApplication.java
@@ -1,9 +1,12 @@
 package com.pedroalmeida.gamebuddy;
 
+import com.pedroalmeida.gamebuddy.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class GamebuddyApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pedroalmeida/gamebuddy/security/AuthenticationController.java
+++ src/main/java/com/pedroalmeida/gamebuddy/security/AuthenticationController.java
@@ -3,6 +3,9 @@ package com.pedroalmeida.gamebuddy.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.pedroalmeida.gamebuddy.appuser.AppUser;
+import com.pedroalmeida.gamebuddy.config.JwtProperties;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,34 +16,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-
-import static com.pedroalmeida.gamebuddy.security.FilterToken.SECRET;
-
 @RestController
 @AllArgsConstructor
 public class AuthenticationController {
+
     private final AuthenticationManager authenticationManager;
     private final AuthenticationService authenticationService;
+    private final JwtProperties jwtProperties;
 
     @PostMapping("/login")
     public String login(@RequestBody Login login) {
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                new UsernamePasswordAuthenticationToken(login.getUsername(), login.getPassword());
+            new UsernamePasswordAuthenticationToken(login.getUsername(), login.getPassword());
 
         Authentication authenticate = this.authenticationManager.authenticate(usernamePasswordAuthenticationToken);
 
         var usuario = (AppUser) authenticate.getPrincipal();
 
         return JWT.create()
-                .withSubject(usuario.getUsername())
-                .withClaim("id", usuario.getUserId())
-                .withExpiresAt(LocalDateTime.now()
-                        .plusMinutes(60)
-                        .toInstant(ZoneOffset.of("+00:00"))
-                )
-                .sign(Algorithm.HMAC256(SECRET));
+            .withSubject(usuario.getUsername())
+            .withClaim("id", usuario.getUserId())
+            .withExpiresAt(LocalDateTime.now()
+                .plusMinutes(60)
+                .toInstant(ZoneOffset.of("+00:00"))
+            )
+            .sign(Algorithm.HMAC256(jwtProperties.getSecret()));
 
     }
 
